f0ddb9b5531b1ca5941ad89df516217d171e66b0
zgmphoo <zgmphoo@gmail.com>
2019-06-01 01:14:09 +0800

    add python example

---
+++ examples/histogram_no_images.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+import pandas as pd
+
+sys.path.append(os.path.dirname(os.getcwd()))
+
+from pgdancer import histogram
+
+
+
+
+if __name__ == '__main__':
+    # example code
+    df = pd.read_csv("brands_data.csv", index_col="brands", thousands=",").fillna(0)
+    df = df.astype("int")
+    h = histogram.Histogram(df, 1280, 720)
+    h.run("pgdancer", "Top 15 Best Global Brands Ranking", "---Datasource:https://www.interbrand.com", \
+          "Top 15 Best Global Brands Ranking", "Brand Value:$m")
