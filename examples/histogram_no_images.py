e42c2a2568d6779c3b2f7cc0c1a25e371b361010
zgmphoo <zgmphoo@gmail.com>
2019-06-02 00:09:49 +0800

    change example's param

--- examples/histogram_no_images.py
+++ examples/histogram_no_images.py
@@ -14,6 +14,6 @@ if __name__ == '__main__':
     # example code
     df = pd.read_csv("brands_data.csv", index_col="brands", thousands=",").fillna(0)
     df = df.astype("int")
-    h = histogram.Histogram(df, 1600, 900, window_type=1)
+    h = histogram.Histogram(df, 1600, 900, window_type=0)
     h.run("pgdancer", "Top 15 Best Global Brands Ranking", "---Datasource:https://www.interbrand.com", \
           "Top 15 Best Global Brands Ranking", "Brand Value:$m")
